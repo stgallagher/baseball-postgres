4f9f08ff9337ce33a18c40da90481895e7032797
stgallagher <s.thomas.gallagher@gmail.com>
2013-10-21 11:14:57 -0700

    first commit

---
+++ app/controllers/gameplay_controller.rb
@@ -0,0 +1,2 @@
+class GameplayController < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BaseballPostgres::Application.config.secret_key_base = '5add983e07a442d11f5fb30edad29a9427a60855339d13f7398998156b087f2ca30a5d6ee95a757fdfac7b81cba406ab7996b92ba3e9679e32439ce178904d7f'
